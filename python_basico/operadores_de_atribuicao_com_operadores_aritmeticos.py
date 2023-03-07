70bad9c711c9335b164774e53542e734477e4140
JessicaPortilio <jessicaket91@gmail.com>
2023-03-07 15:20:57 -0300

    python basico

---
+++ python_basico/operadores_de_atribuicao_com_operadores_aritmeticos.py
@@ -0,0 +1,27 @@
+"""
+Operadores de atribuição
+= += -= *= /= //= **= %=
+"""
+
+contador = 10
+# soma 
+contador += 1 # 11
+print(contador)
+# substrair 
+contador -= 1 #10
+print(contador)
+# multiplicação 
+contador *= 2 #20
+print(contador)
+# divisão
+contador /= 2 #10.0
+print(contador)
+# parte inteira da divisão
+contador //= 2 #5.0
+print(contador)
+# pontenciação
+contador **= 2 #25.0
+print(contador)
+# resto da divisão
+contador %= 2 # 1.0
+print(contador)
--- python_basico/while_estrutura_de_repeticao.py
+++ python_basico/while_estrutura_de_repeticao.py
@@ -15,10 +15,41 @@ Loop infinito -> Quando um código não tem fim
 
 # print('Acabou')
 
-contador = 0
+# contador = 0
 
-while contador <= 10:
-    contador = contador + 1
-    print(contador)
+# while contador <= 10:
+#     contador = contador + 1
+#     print(contador)
 
+# print('Acabou')
+
+# contador = 0
+
+# while contador <= 100:
+#     contador += 1
+
+#     if contador == 6:
+#         print('Não vou mostrar o 6.')
+#         continue
+
+#     if contador >= 10 and contador <= 27:
+#         print('Não vou mostrar o', contador)
+#         continue
+
+#     print(contador)
+
+#     if contador == 40:
+#         break
+
+# print('Acabou')
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
 print('Acabou')
