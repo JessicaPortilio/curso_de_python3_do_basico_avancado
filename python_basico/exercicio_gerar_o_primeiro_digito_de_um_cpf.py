e285cd30402eb1121f6a3512fb7b77248e91e448
JessicaPortilio <jessicaket91@gmail.com>
2023-03-20 08:51:05 -0300

    python basico

--- python_basico/exercicio_gerar_o_primeiro_digito_de_um_cpf.py
+++ python_basico/exercicio_gerar_o_primeiro_digito_de_um_cpf.py
@@ -33,19 +33,69 @@ while(contador >=2):
     soma += int(x) * contador
     contador -=1
 
-print(soma)
+# print(soma)
 
 multiplicar = soma * 10
-print(multiplicar)
+# print(multiplicar)
 resto = 0
 resto = multiplicar % 11
-print(resto)
+# print(resto)
 
 if resto > 9:
   novo_cpf += '0'
-  print(novo_cpf)
+  # print(novo_cpf)
 else:
   novo_cpf += str(resto)
 
+# cpf_formatado = '{}.{}.{}-{}'.format(novo_cpf[:3], novo_cpf[3:6], novo_cpf[6:9], novo_cpf[9:]) 
+# print(cpf_formatado)
+
+"""
+Calculo do segundo dígito do CPF
+CPF: 746.824.890-70
+Colete a soma dos 9 primeiros dígitos do CPF,
+MAIS O PRIMEIRO DIGITO,
+multiplicando cada um dos valores por uma
+contagem regressiva começando de 11
+Ex.:  746.824.890-70 (7468248907)
+   11 10  9  8  7  6  5  4  3  2
+*  7   4  6  8  2  4  8  9  0  7 <-- PRIMEIRO DIGITO
+   77 40 54 64 14 24 40 36  0 14
+Somar todos os resultados:
+77+40+54+64+14+24+40+36+0+14 = 363
+Multiplicar o resultado anterior por 10
+363 * 10 = 3630
+Obter o resto da divisão da conta anterior por 11
+3630 % 11 = 0
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+O segundo dígito do CPF é 0
+"""
+soma2 = 0
+contador2 = 11
+while(contador2 >=2):
+   for x in novo_cpf:
+    soma2 += int(x) * contador2
+    contador2 -=1
+
+# print(soma2)
+
+multiplicar2 = soma2 * 10
+# print(multiplicar2)
+resto2 = 0
+resto2 = multiplicar2 % 11
+# print(resto2)
+
+if resto2 > 9:
+  novo_cpf += '0'
+  # print(novo_cpf)
+else:
+  novo_cpf += str(resto2)
+
 cpf_formatado = '{}.{}.{}-{}'.format(novo_cpf[:3], novo_cpf[3:6], novo_cpf[6:9], novo_cpf[9:]) 
-print(cpf_formatado)
+if cpf_formatado == cpf:
+  print(f'{cpf_formatado} é válido')
+else:
+  print(f'{cpf_formatado} não é válido')
