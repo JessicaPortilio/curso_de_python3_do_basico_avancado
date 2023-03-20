b2ae66f90af7c2462c83a6c5f3915398343af2eb
JessicaPortilio <jessicaket91@gmail.com>
2023-03-20 16:09:02 -0300

    python basico

--- python_basico/exercicio_gerar_o_primeiro_digito_de_um_cpf.py
+++ python_basico/exercicio_gerar_o_primeiro_digito_de_um_cpf.py
@@ -20,11 +20,19 @@ contrário disso:
     resultado é o valor da conta
 O primeiro dígito do CPF é 7
 """
+import sys
 
-cpf = '746.824.890-70'
+cpf = input('Digite o número do cpf [746.824.890-70]: ')
+
 novo_cpf = cpf.replace(".", "").replace("-", "")[:9]
 soma = 0
 multiplicar = 0
+
+entrada_e_sequencial = novo_cpf == novo_cpf[0] * len(novo_cpf)
+
+if entrada_e_sequencial:
+    print('Você enviou dados sequenciais.')
+    sys.exit()
 
 # print(novo_cpf)
 contador = 10
@@ -98,4 +106,4 @@ cpf_formatado = '{}.{}.{}-{}'.format(novo_cpf[:3], novo_cpf[3:6], novo_cpf[6:9],
 if cpf_formatado == cpf:
   print(f'{cpf_formatado} é válido')
 else:
-  print(f'{cpf_formatado} não é válido')
+  print(f'{cpf}, não é válido')
