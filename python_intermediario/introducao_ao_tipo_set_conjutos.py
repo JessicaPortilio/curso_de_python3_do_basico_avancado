dace8d0f5190f2de2fa64fbfd53d2dcd0dec5871
JessicaPortilio <jessicaket91@gmail.com>
2023-03-27 08:10:13 -0300

    python intermediario

--- python_intermediario/introducao_ao_tipo_set_conjutos.py
+++ python_intermediario/introducao_ao_tipo_set_conjutos.py
@@ -37,8 +37,20 @@ s1.update(('Olá mundo', 1, 2, 3, 4))
 s1.discard('Olá mundo') #
 s1.discard('Luiz')
 print(s1)
+
 # Operadores úteis:
 # união | união (union) - Une
 # intersecção & (intersection) - Itens presentes em ambos
 # diferença - Itens presentes apenas no set da esquerda
 # diferença simétrica ^ - Itens que não estão em ambos
+
+s1 = {1, 2, 3}
+s2 = {2, 3, 4}
+s3 = s1 | s2
+print(f'União s1 | s2 une: ', s3)
+s3 = s1 & s2
+print(f'intersecção - Itens presentes em ambos: ', s3)
+s3 = s2 - s1
+print(f'Diferença - Itens presentes apenas no set da esquerda: ', s3)
+s3 = s1 ^ s2
+print(f'diferença simétrica - Itens que não estão em ambos', s3)
