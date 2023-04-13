a6ee59bb461619e706e33a17fca13bc43d7d61d6
JessicaPortilio <jessicaket91@gmail.com>
2023-04-12 21:16:40 -0300

    python intermediario

--- python_intermediario/count_e_um_iterador_sem_fim.py
+++ python_intermediario/count_e_um_iterador_sem_fim.py
@@ -2,13 +2,13 @@
 
 from itertools import count
 
-c1 = count()
-r1 = range(10)
+c1 = count(16, 8) #inicia no 16 pegando só os multiplicos de 8
+r1 = range(16, 100, 8) #inicia no 16 até o 100 pegando só os multiplicos de 8
 
-print('c1', hasattr(c1, '__iter__')) # count é interavel
-print('c1', hasattr(c1, '__next__')) # count é intereto
-print('r1', hasattr(r1, '__iter__')) # range é interavel
-print('r1', hasattr(r1, '__next__')) # range não é intereto
+# print('c1', hasattr(c1, '__iter__')) # count é interavel
+# print('c1', hasattr(c1, '__next__')) # count é intereto
+# print('r1', hasattr(r1, '__iter__')) # range é interavel
+# print('r1', hasattr(r1, '__next__')) # range não é intereto
 
 print('count')
 for i in c1:
@@ -17,7 +17,7 @@ for i in c1:
     print(i)
 
 print()
-print('range')
 
+print('range')
 for i in r1:
     print(i)
